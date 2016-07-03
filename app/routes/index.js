d391ef1104940d5e23d4108957d5ed8095f64082
Tania <tatashat@gmail.com>
2016-07-03 18:51:06 +0000

    * timestamp route

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,9 +16,33 @@ module.exports = function (app, passport) {
 			res.sendFile(path + '/public/index.html');
 		});
 		
+	app.route('/favicon.ico')
+		.get(function (req, res) {
+			res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        	res.end();
+		});
+		
 	app.route('/:timestamp')
 		.get(function (req, res) {
-			res.json({});
+			var timestamp = req.params.timestamp;
+			
+			var date = new Date(timestamp*1000) || Date.parse(timestamp);
+			
+			console.log(new Date(timestamp*1000))
+			
+			if(!date) {
+				res.json(null);
+				return;
+			}
+			
+			console.log(date)
+			var unix = Math.round(date.getTime()/1000);
+			var antural = date.toDateString();
+			
+			res.json({ 
+				"unix": unix, 
+				"natural": antural
+			});
 		});
 
 	/*app.route('/login')
