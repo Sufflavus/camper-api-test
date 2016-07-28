acd6cb57626262edcf080c2e88851fb3023c5b60
Tania Shatilova <tatashat@gmail.com>
2016-07-28 19:11:49 +0200

    - files from timestamp api

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 var express = require('express');
-
-var path = process.cwd();
-var TimestampHandler = require(path + '/app/timestamp-handler.js');
-
 var app = express();
-var timestampHandler = new TimestampHandler();
 
 app.get('/', function (req, res) {
-  res.sendFile(path + '/public/index.html');
+  res.send('Hello World!');
 });
-
-app.get('/favicon.ico', function (req, res) {
-});
-
-app.get('/:timestamp', timestampHandler.getTime);
 
 app.listen(8080, function () {
-  console.log('Timestamp app listening on port 8080!');
+  console.log('Example app listening on port 3000!');
 });
