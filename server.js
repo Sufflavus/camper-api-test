6758376d120f10338181bdf2672b9cff86114e37
Tania <tatashat@gmail.com>
2016-07-28 04:54:33 +0000

    + datetime converter

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var express = require('express');
 
 var path = process.cwd();
-var TimeHandler = require(path + '/app/timeHandler.server.js');
+var TimestampHandler = require(path + '/app/timestamp-handler.js');
 
 var app = express();
-var timeHandler = new TimeHandler();
+var timestampHandler = new TimestampHandler();
 
 app.get('/', function (req, res) {
   res.sendFile(path + '/public/index.html');
 });
 
-app.get('/:timestamp', timeHandler.getTime);
+app.get('/favicon.ico', function (req, res) {
+});
+
+app.get('/:timestamp', timestampHandler.getTime);
 
 app.listen(8080, function () {
   console.log('Timestamp app listening on port 8080!');
